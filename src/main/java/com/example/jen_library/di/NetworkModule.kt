d5cfba89c24e80987ba9cf6bc98c20b6f34489c0
Sumin <jennifer045020@gmail.com>
2020-03-11 00:18:06 +0900

    NetworkModule

---
+++ src/main/java/com/example/jen_library/di/NetworkModule.kt
@@ -0,0 +1,33 @@
+package com.example.jen_library.di
+
+import androidx.databinding.library.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val TIMEOUT: Long = 15
+
+fun getNetworkModule(baseUrl: String) = module {
+    single {
+        val httpLoggingInterceptor = HttpLoggingInterceptor()
+        httpLoggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
+        OkHttpClient().newBuilder()
+            .connectTimeout(TIMEOUT, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    single { GsonConverterFactory.create() as Converter.Factory }
+
+    single {
+        Retrofit.Builder()
+            .client(get())
+            .addConverterFactory(get())
+            .baseUrl(baseUrl)
+            .build()
+    }
+}
