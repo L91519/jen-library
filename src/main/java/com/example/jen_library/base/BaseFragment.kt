a2319ba5e85dfc93e118679521bd51617ccb7b85
Sumin <jennifer045020@gmail.com>
2020-03-11 00:17:57 +0900

    BaseFragment

---
+++ src/main/java/com/example/jen_library/base/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.example.jen_library.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(
+    @LayoutRes val layoutResId: Int
+) : Fragment() {
+    protected lateinit var binding: B
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        return binding.root
+    }
+}
