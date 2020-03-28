91d4325ea529bd5e4ff3743585663ba8fb835a6b
Sumin <jennifer045020@gmail.com>
2020-03-28 11:27:11 +0900

    Base Activity 수정

--- src/main/java/com/example/jen_library/base/BaseActivity.kt
+++ src/main/java/com/example/jen_library/base/BaseActivity.kt
@@ -1,7 +1,10 @@
 package com.example.jen_library.base
 
+import android.os.Bundle
 import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
+import com.example.jen_library.BR
 
 abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>(
     @LayoutRes private val layoutRes: Int
@@ -10,4 +13,11 @@ abstract class BaseActivity<B : ViewDataBinding, VM : BaseViewModel>(
     protected lateinit var binding: B
     protected abstract val vm: VM
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, layoutRes)
+        binding.setVariable(BR.vm, vm)
+        binding.lifecycleOwner = this
+    }
 }
